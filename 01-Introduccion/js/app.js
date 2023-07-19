5544975cbd0d50a36143710ea15c9dd97604ae34
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-07-19 09:52:33 -0500

    Commit inicial

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,5 @@
+// Toma el nombre al cargar la página
+const nombre = prompt('¿Cuál es tu nombre?');
+
+// Coloca el nombre en la página
+document.querySelector('.hashtag').innerHTML = `${nombre} está aprendiendo JavaScript`;
