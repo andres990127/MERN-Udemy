79ae23b3fdf597aa7abffc71a281150735a99134
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-07-19 14:12:06 -0500

    Operadores lógicos

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,10 @@
+// Undefined 
+let numero1;
+console.log(numero1);
+
+// Null
+let numero2 = null;
+console.log(numero2);
+
+console.log( numero1 == numero2 ); // True
+console.log( numero1 === numero2 ); // False
