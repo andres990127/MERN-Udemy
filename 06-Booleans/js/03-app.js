6029a32bf3f3b33468aa3c368c8c483f605c7cb8
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-07-19 14:48:50 -0500

    Booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+if( autenticado ){
+    console.log('Si puedes ver netflix');
+}
+else{
+    console.log('No puedes ver netflix')
+}
+
+// Operador ternario
+console.log( autenticado ? 'Si está autenticado' : 'No está autenticado');
